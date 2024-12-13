6321d76842b36dc54f61d495e3459592e18552df
socheatathai <socheatathai62@gmail.com>
2024-12-13 11:13:35 +0700

    let start with vue js

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data: () => ({
+    jsFrameworks: ["react.js", "vue.js", "angular"],
+    backendFrameworks: [],
+  }),
+  methods: {
+    handleChange(tags) {
+      this.jsFrameworks = [...tags];
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Your favorite frameworks</h1>
+  <div>{{ jsFrameworks }}</div>
+  <tags-input :selected-tags="jsFrameworks" @change="handleChange" />
+
+  <div>{{ backendFrameworks }}</div>
+  <tags-input
+    :selected-tags="backendFrameworks"
+    @change="backendFrameworks = [...$event]"
+  />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  color: blue;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import TagsInput from "./components/TagsInput.vue";
+
+const app = createApp(App);
+app.component("tags-input", TagsInput);
+app.mount("#app");
